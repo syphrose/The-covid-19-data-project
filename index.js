36b3de4340b7b45b7981910b1e5492ac83da9a89
syphrose <syphrose.wabulwenyi@student.moringaschool.com>
2022-06-23 22:59:51 +0300

    fetched info from the API

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 fetch('https://covid-19.dataflowkit.com/v1/usa')
 .then(res=>res.json())
 .then(data=>console.log(data))
+
+let fetchStatistics =async() => {
+    let statistics = await fetch('https://covid-19.dataflowkit.com/v1/usa')
+    let data = await statistics.json()
+    console.log(data.items.volumeInfo)
+    let response = data.items
+    response.map((statisticsdata) => {
+        console.log(statisticsdata)
+    })
+
+      const list = document.querySelector('.statistics-display.list')
+      const li= document.createElement("li");
+      li.textContent = statisticsdata.volumeInfo.title
+      list.appendChild(li);
+}
+
+
+
+fetchStatistics()
