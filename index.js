fd0d88f9ebddf9c64794a226be8215a6a59c4851
syphrose <syphrose.wabulwenyi@student.moringaschool.com>
2022-06-24 19:56:52 +0300

    decribed the form launch and closing form

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '8fd1e108cdmshbd7af98f47bd8c0p117edejsndc62622f656a',
-		'X-RapidAPI-Host': 'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com'
-	}
-};
+document.addEventListener('DOMContentLoaded', ()=> {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '8fd1e108cdmshbd7af98f47bd8c0p117edejsndc62622f656a',
+      'X-RapidAPI-Host': 'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com'
+    }
+  };
 
-// fetch('https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
+  // fetch('https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/', options)
+  // 	.then(response => response.json())
+  // 	.then(response => console.log(response))
+  // 	.catch(err => console.error(err));
 
-let fetchStatistics =async() => {
-    let resp = await fetch('https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/', options)
-    let data = await resp.json()
-    console.log(data)
-    let response = data
-    response.map((stats) => {
-        // console.log(stats.country)
-    
-      const list = document.querySelector('.container ul')
-      const li= document.createElement("li");
-      li.textContent = `${stats.Country} total deaths: ${stats.TotalDeaths}`
-      list.appendChild(li);
+  let fetchStatistics =async() => {
+      let resp = await fetch('https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/', options)
+      let data = await resp.json()
+      console.log(data)
+      let response = data
+      response.map((stats) => {
+          // console.log(stats.country)
+      
+        const list = document.querySelector('.container ul')
+        const li= document.createElement("li");
+        li.textContent = `${stats.Country} total deaths: ${stats.TotalDeaths}`
+        list.appendChild(li);
 
-    })
+      })
 
 
-  }
-
-document.addEventListener('DOMContentLoaded', ()=> {fetchStatistics()})
-
+    }
+  fetchStatistics()
+})
 function openlogin(){
   document.getElementById('formlogin').style.display = "block"
 }
